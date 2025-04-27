2c566cb291e412cbd36bee129b90d77186ca0ebd
adewaleijalana <adewaleijalana@gmail.com>
2025-04-27 12:54:34 +0100

    log_all_pairs_in_array

--- big-0/src/main.rs
+++ big-0/src/main.rs
@@ -1,13 +1,23 @@
+#![allow(dead_code, unused_imports)]
+
 use std::time::Instant;
 
-fn main() {
-    let names = vec!("nemo", "titi", "toto", "nemo", "tata", "nemo");
-    let time = Instant::now();
-    find_nemo(&names);
-    let time_elapsed = time.elapsed();
+mod arrays;
 
-    println!("Time elapsed: {:?}", time_elapsed);
-    println!("Time elapsed: {:?}", time_elapsed.as_millis());
+use arrays::log_all_pairs_in_array;
+
+fn main() {
+    // let names = vec!("nemo", "titi", "toto", "nemo", "tata", "nemo");
+    // let time = Instant::now();
+    // find_nemo(&names);
+    // let time_elapsed = time.elapsed();
+
+    // println!("Time elapsed: {:?}", time_elapsed);
+    // println!("Time elapsed: {:?}", time_elapsed.as_millis());
+
+    let arrs = vec![1, 2, 3, 4, 5];
+
+    log_all_pairs_in_array(&arrs);
 }
 
 fn find_nemo(names: &Vec<&str>){
