6e34b8c7c039e17f72a39c592365f783df7eb1d6
adewaleijalana <adewaleijalana@gmail.com>
2025-05-03 00:25:44 +0100

    pairs in array using a set

--- big-0/src/arrays.rs
+++ big-0/src/arrays.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display, hash::Hash};
+use std::{collections::{HashMap, HashSet}, fmt::Display, hash::Hash};
 
 pub fn log_all_pairs_array(arrs: &[&[i32]]) {
     for i in 0..arrs.len() {
@@ -30,4 +30,32 @@ pub fn contains_common_items<T : Eq + Hash>(arr1: &[T], arr2: &[T]) -> bool{
         }
     }
     false
+}
+
+pub fn all_pairs_in_array(arrs: &[i32], sum: i32) {
+    for i in 0..arrs.len() {
+        for j in 0..arrs.len() {
+            if arrs[i] + arrs[j] == sum {
+                println!("Pair: ({}, {})", arrs[i], arrs[j]);
+            }
+        }
+    }
+    
+}
+
+pub fn all_pairs_in_array_set(arrs: &[i32], sum: i32) -> bool{
+    let mut pair_set = HashSet::new();
+    for i in 0..arrs.len() {
+        // if pair_set.contains(&(sum - arrs[i])) {
+        if pair_set.contains(&arrs[i]) {
+            println!("Pair: ({}, {})", arrs[i], sum - arrs[i]);
+            return true;
+        } else {
+            // pair_set.insert(arrs[i]);
+            pair_set.insert(sum - arrs[i]);
+            
+        }
+    }
+    false
+    
 }
--- big-0/src/main.rs
+++ big-0/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 mod arrays;
 
-use arrays::{log_all_pairs_in_array, contains_common_items};
+use arrays::{log_all_pairs_in_array, contains_common_items, all_pairs_in_array_set};
 
 fn main() {
     // let names = vec!("nemo", "titi", "toto", "nemo", "tata", "nemo");
@@ -15,20 +15,29 @@ fn main() {
     // println!("Time elapsed: {:?}", time_elapsed);
     // println!("Time elapsed: {:?}", time_elapsed.as_millis());
 
-    // let arrs = vec![1, 2, 3, 4, 5];
+    let arrs = vec![1, 2, 3, 4, 5];
 
     // log_all_pairs_in_array(&arrs);
 
-    let arrs1 = vec!['a', 'b', 'c', 'f', 'g'];
+    // let arrs1 = vec!['a', 'b', 'c', 'f', 'g'];
 
-    let arrs2 = vec!['w', 'i', 'z', 't', 'u'];
+    // let arrs2 = vec!['w', 'i', 'z', 't', 'u'];
 
-    let is_contains = contains_common_items(&arrs1, &arrs2);
-    if is_contains {
-        println!("Contains common items");
+    // let is_contains = contains_common_items(&arrs1, &arrs2);
+    // if is_contains {
+    //     println!("Contains common items");
+    // } else {
+    //     println!("Does not contain common items");
+    // }
+
+    let found_pairs = all_pairs_in_array_set(&arrs, 5);
+    if found_pairs {
+        println!("Contains pairs");
     } else {
-        println!("Does not contain common items");
+        println!("Does not contain pairs");
     }
+
+    
 }
 
 fn find_nemo(names: &Vec<&str>){
